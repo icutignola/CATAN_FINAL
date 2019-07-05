874d12a3dd291c1ea5115f11334e48ea660e19bb
Thomas Neuhaus <thomneuhaus98@live.com.ar>
2019-07-05 12:52:56 -0300

    eventgen1

--- CATAN_FINAL/CATAN_FINAL/eventGen.cpp
+++ CATAN_FINAL/CATAN_FINAL/eventGen.cpp
@@ -1,9 +1,51 @@
 #include "eventGen.h"
 
 
-
 eventGen::eventGen()
 {
+
+}
+
+genericEvent *eventGen::
+getEvent()
+{
+	genericEvent * reEV = nullptr;
+	
+	if (GUI->isInput() == EV_EXIST)
+	{
+		input = GUI->getInput();
+		switch (input)
+		{
+		case OFFER_TRADE:
+			reEV = new cTrade;
+			break;
+		case PORT_TRADE:
+			reEV = new cPortTrade;
+			break;
+		case BANK_TRADE:
+			reEV = new cBankTrade;
+			break;
+		case BUILD:
+			reEV = new cBuild;
+			break;
+			/*case MaxScore:
+				reEV = new cMaxScore;
+				break;
+			case Invalid:
+				reEV = new cInvalid;
+				break;*/
+		case PASS:
+			reEV = new cEndOfTurn;
+			break;
+		case DICES_ARE:
+			reEV = new cDiceThrew;
+			break;
+		case QUIT:
+			reEV = new cEventQuit;
+			break;
+		}
+	}
+	return reEV;
 }
 
 
--- CATAN_FINAL/CATAN_FINAL/eventGen.h
+++ CATAN_FINAL/CATAN_FINAL/eventGen.h
@@ -1,64 +1,81 @@
 #pragma once
 #include "eventHandling.h"
+#include "defines.h"
+#include "auxiliar.h"
+#include "io.h"
+#include "client.h"
+#include "server.h"
 
 enum implEvent : eventTypes { Trade, PortTrade, BankTrade, Build, MaxScore, Invalid, EndOfTurn, DiceThrew, EventQuit };
 
 class cTrade : public genericEvent
 {
 public:
-	eventTypes getType() override { return Trade; }
+	eventTypes getType()  { return Trade; }
 };
 
 class cPortTrade : public genericEvent
 {
 public:
-	eventTypes getType() override { return PortTrade; }
+	eventTypes getType()  { return PortTrade; }
 };
 
 class cBankTrade : public genericEvent
 {
 public:
-	eventTypes getType() override { return BankTrade; }
+	eventTypes getType()  { return BankTrade; }
 };
 
 class cBuild : public genericEvent
 {
 public:
-	eventTypes getType() override { return Build; }
+	eventTypes getType()  { return Build; }
 };
 
 class cMaxScore : public genericEvent
 {
 public:
-	eventTypes getType() override { return MaxScore; }
+	eventTypes getType()  { return MaxScore; }
 };
 
 class cInvalid : public genericEvent
 {
 public:
-	eventTypes getType() override { return Invalid; }
+	eventTypes getType()  { return Invalid; }
 };
 
 class cEndOfTurn : public genericEvent
 {
 public:
-	eventTypes getType() override { return EndOfTurn; }
+	eventTypes getType()  { return EndOfTurn; }
 };
 
 class cDiceThrew : public genericEvent
 {
 public:
-	eventTypes getType() override { return DiceThrew; }
+	eventTypes getType()  { return DiceThrew; }
 };
 
 class cEventQuit : public genericEvent
 {
 public:
-	eventTypes getType() override { return EventQuit; }
-class eventGen
-{
-public:
-	eventGen();
-	~eventGen();
+	eventTypes getType() { return EventQuit; }
 };
 
+class eventGen
+{
+private:
+	io* GUI;
+	client* COMU_c;
+	server* COMU_s;
+	message mensaje;
+	int myStatus;		// Mi estado en la comunicacion
+	bool messageExist;
+	bool ackReceived;
+	unsigned char input;
+
+public:
+	eventGen();
+	genericEvent * getEvent();
+	~eventGen();
+};
